2bab3b39bb8cfef4ac0cbfb89a32dc958ad4895c
Lyall <lalwafi@Mac.lan>
2026-05-18 21:22:06 +0400

    polyset changes

--- new_examRank5/level_0/polyset/set.hpp
+++ new_examRank5/level_0/polyset/set.hpp
@@ -1,23 +1,21 @@
-#pragma once
-#include "searchable_bag.hpp"
-
 class set
 {
 	private:
-		searchable_bag *_bag;
+		searchable_bag& bag;
 	public:
-		set();
-		set(searchable_bag &bag);
-		set(const set &copy);
-		set	&operator=(const set &copy);
-		virtual ~set();
+		set() = delete;
+		set(const set& source) = delete;
+		set& operator=(const set& source) = delete;
+		set(searchable_bag& s_bag);
 
-		searchable_bag &get_bag() const;
-
-		void insert(int);
-		void insert(int *, int);
+		bool has(int) const;
+		void insert (int);
+		void insert (int *, int);
 		void print() const;
 		void clear();
 
-		bool has(int) const;
+		const searchable_bag& get_bag();
+
+		~set();
+
 };
